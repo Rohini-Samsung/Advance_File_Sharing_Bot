923534ce5f17df9dc61c467d9fc2dbe14f1b26d1
Gagan Singh <141204441+GaganHonor@users.noreply.github.com>
2023-08-13 13:21:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ class Bot(Client):
 
         if FORCE_SUB_CHANNELS:
              self.invitelink = []
-        for channel_id in FORCE_SUB_CHANNELS:
+           for channel_id in FORCE_SUB_CHANNELS:
         try:
             link = (await self.get_chat(channel_id)).invite_link
         if not link:
