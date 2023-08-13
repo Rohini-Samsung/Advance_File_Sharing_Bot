d294bfdb0a234ee47d6cdda0b34433c271304d24
Gagan Singh <141204441+GaganHonor@users.noreply.github.com>
2023-08-13 11:14:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,9 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://oceanbot:ocean@cluster0.h
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001940711750"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001952741671"))
+#FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001940711750"))
+# Force sub channel ids, if you want to enable force sub
+FORCE_SUB_CHANNELS = [int(channel_id) for channel_id in os.environ.get("FORCE_SUB_CHANNEL", "-1001952741671,-1001952741671").split(",")]
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
